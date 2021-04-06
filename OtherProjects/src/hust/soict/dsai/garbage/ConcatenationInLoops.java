fa8f14922414d39fb96c3f12d2c2e75ac35322fd
Nguyen Huy Hoang <huyhoang240101@users.noreply.github.com>
2021-04-06 14:40:51 +0700

    memory management string

--- OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
+++ OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
@@ -3,8 +3,26 @@ package hust.soict.dsai.garbage;
 public class ConcatenationInLoops {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		long startTime1 = System.currentTimeMillis();
+		String string = new String("Hello");
+		for (int i = 0; i < 100000; i++) {
+			string += " World";
+		}
+		System.out.println("String processing time is: " + (System.currentTimeMillis() - startTime1));
+		
+		long startTime2 = System.currentTimeMillis();
+		StringBuffer buffer = new StringBuffer("Hello");
+		for (int i = 0; i < 100000; i++) {
+			buffer.append(" World");
+		}
+		System.out.println("StringBuffer processing time is: " + (System.currentTimeMillis() - startTime2));
+		
+		long startTime3 = System.currentTimeMillis();
+		StringBuilder builder = new StringBuilder("Hello");
+		for (int i = 0; i < 100000; i++) {
+			builder.append(" World");
+		}
+		System.out.println("StringBuilder processing time is: " + (System.currentTimeMillis() - startTime3));
 	}
 
 }
