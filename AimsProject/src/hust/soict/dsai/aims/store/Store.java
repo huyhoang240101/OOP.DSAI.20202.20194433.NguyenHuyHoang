42c94d837664a944ceb875db67a06f7a1b239d7f
Nguyen Huy Hoang <huyhoang240101@users.noreply.github.com>
2021-04-30 02:36:07 +0700

    update store

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,29 +1,26 @@
 package hust.soict.dsai.aims.store;
 import java.util.ArrayList;
-
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
 
 public class Store {
-	private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+	private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 	
-	//add dvd to the store
-	public void addDVD(DigitalVideoDisc ... dvd) {
-		for (int i = 0; i < dvd.length; i++) {
-			itemsInStore.add(dvd[i]);
+	//add items to the store
+	public void addMediaToStore(Media ... media) {
+		for (int i = 0; i < media.length; i++) {
+			itemsInStore.add(media[i]);
 		}
-		System.out.println("\n" + dvd.length + " disc has been added to the store.");
+		System.out.println("\n" + media.length + " items has been added to the store.");
 	}
 	
-	//remove dvd from the store
-	public void removeDVD(DigitalVideoDisc ... dvd) {
-		for (int i = 0; i < dvd.length; i++) {
-			for (int j = 0; j < itemsInStore.size(); j++) {
-				if (dvd[i] == itemsInStore.get(j)) {
-					itemsInStore.remove(j);
-				}
+	//remove items from the store
+	public void removeMediaFromStore(Media ... media) {
+		for (int i = 0; i < media.length; i++) {
+			if (itemsInStore.contains(media[i]) == true) {
+				itemsInStore.remove(media[i]);
 			}
 		}
-		System.out.println( "\n"+ dvd.length + " disc has been removed from the store.");
+		System.out.println( "\n"+ media.length + " items has been removed from the store.");
 	}
 	public void printStore() {
 		System.out.println("************************STORE****************************************");
