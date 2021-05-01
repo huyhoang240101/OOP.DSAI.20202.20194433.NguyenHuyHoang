76a14e8d6e4ca90561a5bba59bc05ca593cb3134
Nguyen Huy Hoang <huyhoang240101@users.noreply.github.com>
2021-05-01 15:15:39 +0700

    hot fix

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,45 +1,18 @@
 package hust.soict.dsai.aims;
-
+import java.time.LocalDate;
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
 public class Aims {
 
-	public static void main(String[] args) {
-		Cart an0ther = new Cart();
-		
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		
-		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Avengers", "Science Fiction", 20.00f);
-		
-		DigitalVideoDisc dvd5 = new DigitalVideoDisc("Toy Story", "Animation", 15.25f);
-		
-		DigitalVideoDisc dvd6 = new DigitalVideoDisc("Spiderman", "Science Fiction", 15.50f);
-		
-		DigitalVideoDisc dvd7 = new DigitalVideoDisc("Coco", "Animation", 16.00f);
-		
-		DigitalVideoDisc dvd8 = new DigitalVideoDisc("Soul", "Animation", 16.25f);
-		
-		DigitalVideoDisc dvd9 = new DigitalVideoDisc("Star Trek", "Science Fiction", 15.75f);
-		
-		DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3};
-		
-		an0ther.addDigitalVideoDisc(dvdList);
-		an0ther.addDigitalVideoDisc(dvd4,dvd5);
-		an0ther.addDigitalVideoDisc(dvd6, dvd7, dvd8, dvd9);
-		
-		System.out.println("Total Cost is: ");
-		System.out.println(an0ther.totalCost());
-		
-		an0ther.removeDigitalVideoDisc(dvd2);
-		System.out.println("Total Cost is: ");
-		System.out.println(an0ther.totalCost());
-		
-		an0ther.printCart(an0ther);
+	public static void showMenu() {
+		System.out.println("AIMS: ");
+		System.out.println("--------------------------------");
+		System.out.println("1. View store");
+		System.out.println("2. Update store");
+		System.out.println("3. See current cart");
+		System.out.println("0. Exit");
+		System.out.println("--------------------------------");
+		System.out.println("Please choose a number: 0-1-2-3");
 	}
-
 }
--- AimsProject/src/hust/soict/dsai/aims/cart/MediaComparatorByTitleCost.java
+++ AimsProject/src/hust/soict/dsai/aims/cart/MediaComparatorByTitleCost.java
@@ -2,7 +2,7 @@ package hust.soict.dsai.aims.cart;
 import java.util.Comparator;
 import hust.soict.dsai.aims.media.Media;
 
-public class MediaComparatorByTitleCost {
+public class MediaComparatorByTitleCost implements Comparator<Media> {
 	public int compare(Media o1, Media o2) {
 		int CostCompare = (int) (o2.getCost() - o1.getCost());
 		int TitleCompare = o1.getTitle().compareTo(o2.getTitle());
