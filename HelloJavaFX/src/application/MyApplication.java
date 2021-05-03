a9b1221eb8c53a4d369a09b5a51e18e3ea9b3b55
Nguyen Huy Hoang <huyhoang240101@users.noreply.github.com>
2021-05-04 02:40:12 +0700

    light exercise

---
+++ HelloJavaFX/src/application/MyApplication.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+
+public class MyApplication extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/MyScene.fxml"));
+			
+			primaryStage.setTitle("My Application");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
